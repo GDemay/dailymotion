0c903e0251425a42887ba0c5106f4ed44b6ce251
Guillaume Demay <guillaumedemay@hotmail.fr>
2022-07-31 17:03:36 +0200

    feat: change names

--- code/app/api/api_v1/endpoints/login.py
+++ code/app/api/api_v1/endpoints/login.py
@@ -17,8 +17,8 @@ from sqlalchemy.orm import Session
 router = APIRouter()
 
 
-@router.post("/access-token", response_model=schemas.Token)
-def login_access_token(
+@router.post("/auth", response_model=schemas.Token)
+def auth(
     db: Session = Depends(deps.get_db), form_data: OAuth2PasswordRequestForm = Depends()
 ) -> Any:
     """
@@ -28,8 +28,9 @@ def login_access_token(
         db, email=form_data.username, password=form_data.password
     )
     if not user:
-        raise HTTPException(status_code=400, detail="Incorrect email or password")
+        raise HTTPException(status_code=401, detail="Unauthorized. Incorrect email or password")
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    #TODO Create a schema
     return {
         "access_token": create_access_token(
             user.id, expires_delta=access_token_expires
@@ -38,7 +39,7 @@ def login_access_token(
     }
 
 
-@router.post("/test-token", response_model=schemas.User)
+@router.post("/me", response_model=schemas.User)
 def test_token(current_user: models.User = Depends(deps.get_current_user)) -> Any:
     """
     Check token access
@@ -93,7 +94,7 @@ def email_validator(
     return Token
 
 
-@router.post("/token-validator")
+@router.post("/activate-user")
 def token_validator(
     current_user: models.User = Depends(deps.get_current_user),
     db: Session = Depends(deps.get_db),
--- code/app/schemas/__init__.py
+++ code/app/schemas/__init__.py
@@ -1,3 +1,2 @@
-from .msg import Msg
 from .token import Token, TokenCreate, TokenPayload, TokenUpdate
 from .user import User, UserCreate, UserInDB, UserUpdate
